45f2051db75b084e9afdf819d26dcf1b584cab51
jein4433 <jein4433@gmail.com>
2024-07-15 21:40:11 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,40 +1,13 @@
-var mapContainer = document.getElementById('map'), 
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = { 
-        center: new kakao.maps.LatLng(37.5665, 126.9780), // 지도 중심 좌표 (서울 시청)
-        level: 3 // 지도 확대 레벨
+        center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
     };
-var map = new kakao.maps.Map(mapContainer, mapOption);
 
-// 출발지와 도착지 설정
-var start = new kakao.maps.LatLng(37.5665, 126.9780); // 서울 시청
-var end = new kakao.maps.LatLng(37.5770, 126.9769); // 경복궁
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
-// 출발지와 도착지 마커 생성
-var markerStart = new kakao.maps.Marker({
-    position: start
-});
-var markerEnd = new kakao.maps.Marker({
-    position: end
-});
+// 지도에 교통정보를 표시하도록 지도타입을 추가합니다
+map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);    
 
-markerStart.setMap(map);
-markerEnd.setMap(map);
-
-// 두 지점 간의 거리 계산
-var distance = kakao.maps.services.Util.getDistance(start, end);
-
-// 거리 결과를 표시
-var resultDiv = document.getElementById('result');
-resultDiv.innerHTML = '출발지와 도착지 간의 거리: ' + (distance / 1000).toFixed(2) + ' km';
-
-// 지도에 경로 표시
-var linePath = [start, end];
-var polyline = new kakao.maps.Polyline({
-    path: linePath,
-    strokeWeight: 5,
-    strokeColor: '#FF0000',
-    strokeOpacity: 1.0,
-    strokeStyle: 'solid'
-});
-
-polyline.setMap(map);
+// 아래 코드는 위에서 추가한 교통정보 지도타입을 제거합니다
+// map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);  
