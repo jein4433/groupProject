43adebe9b619169c6e218266a3348e6701623ef9
jein4433 <jein4433@gmail.com>
2024-07-16 15:54:51 +0900

    Update main.js

--- main.js
+++ main.js
@@ -211,16 +211,3 @@ function onOffSearch() {
         searchEnd.style.display = 'none';
     }
 }
-
-function searchPubTransPathAJAX() {
-	var xhr = new XMLHttpRequest();
-	var url = "https://api.odsay.com/v1/api/searchPubTransPathT?SX=126.9027279&SY=37.5349277&EX=126.9145430&EY=37.5499421&apiKey={YOUR_API_KEY}";
-	xhr.open("GET", url, true);
-	xhr.send();
-	xhr.onreadystatechange = function() {
-
-		if (xhr.readyState == 4 && xhr.status == 200) {
-			console.log( xhr.responseText ); // <- xhr.responseText 로 결과를 가져올 수 있음
-		}
-	}
-}
