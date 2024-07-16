b86dbe21ae56c379613cf584ecdd3ffe619d7671
jein4433 <jein4433@gmail.com>
2024-07-16 15:44:39 +0900

    Update main.js

--- main.js
+++ main.js
@@ -211,3 +211,91 @@ function onOffSearch() {
         searchEnd.style.display = 'none';
     }
 }
+
+function searchPubTransPathAJAX() {
+	var xhr = new XMLHttpRequest();
+	var url = "https://api.odsay.com/v1/api/searchPubTransPathT?SX=126.9027279&SY=37.5349277&EX=126.9145430&EY=37.5499421&apiKey={YOUR_API_KEY}";
+	xhr.open("GET", url, true);
+	xhr.send();
+	xhr.onreadystatechange = function() {
+
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			console.log( xhr.responseText ); // <- xhr.responseText 로 결과를 가져올 수 있음
+		}
+	}
+}
+
+{
+"result":{
+  "path":[{
+       "pathType":1,
+       "subPath":[{
+         "trafficType":1,
+         "distance":2000,
+         "sectionTime":9,
+         "lane":{
+           "name":"수도권 2호선",
+           "subwaycode":2,
+           "subwayCityCode":1000
+             },
+           "stationCount":1,
+           "passStopList":{
+           "stations":[{
+             "index":0,
+             "stationName":"당산",
+             "stationID":237,
+             "x":126.902682,
+             "y":37.534863
+             },
+             {
+             "index":1,
+             "stationName":"합정",
+             "stationID":238,
+             "x":126.914543,
+             "y":37.549942
+             }]
+          },
+      "way":"합정",
+      "wayCode":2,
+      "startX":126.90267725576584,
+      "startY":37.534862332279936,
+      "startID":237,
+      "startName":"당산",
+      "endExitNo":"",
+      "endExitX":0.0,
+      "endExitY":0.0,
+      "endX":126.91454311097073,
+      "endY":37.549938143345756,
+      "endID":238,
+      "endName":"합정"
+    },
+    …(중략)...
+    ],
+      "info":{
+      "mapObj":"2:2:237:238",
+      "payment":1250,
+      "busTransitCount":0,
+      "subwayTransitCount":1,
+      "busStationCount":0,
+      "subwayStationCount":1,
+      "totalStationCount":1,
+      "totalTime":9,
+      "totalWalk":0,
+      "trafficDistance":2000,
+      "totalDistance":2000,
+      "firstStartStation":"당산",
+      "lastEndStation":"합정",
+      "totalWalkTime":-1
+    }
+    }],
+    "searchType":0,
+    "startRadius":700,
+    "endRadius":700,
+    "subwayCount":1,
+    "busCount":10,
+    "subwayBusCount":0,
+    "pointDistance":1967,
+    "outTrafficCheck":0
+  }
+}
+
